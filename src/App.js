a7b2480d521a031fecb15778afa202c5bd80291d
Sayam Ajmal <itssanesayam@gmail.com>
2022-06-13 11:19:15 +0100

    2 input field with button fully working useRef

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import useRandomJoke from "./useRandomJoke";
 
 function App() {
-  const joke = useRandomJoke("amad", "butt");
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+
+  const [firstName, setFirstName] = useState("sayam");
+  const [lastName, setLastName] = useState("ajmal");
+
+  const joke = useRandomJoke(firstName, lastName);
 
   //custom hook
   //const joke = useRandomJoke("sayam", "ajmal");
 
-  
+  const generateJoke = (e) => {
+    e.preventDefault();
+    setFirstName(firstNameRef.current.value);
+    setLastName(lastNameRef.current.value);
+  };
+
   return (
     <div>
       <center>
         <h1>The Joke Generator</h1>
         <h2>{joke}</h2>
-        <button>Generate Joke</button>
+
+        <form>
+          <input placeholder="first name" ref={firstNameRef} />
+          <input placeholder="last name" ref={lastNameRef} />
+
+          <button onClick={generateJoke}>Generate Joke</button>
+        </form>
       </center>
     </div>
   );
@@ -22,6 +40,5 @@ function App() {
 export default App;
 
 // http://api.icndb.com/jokes/random
-
 
 //8:39
